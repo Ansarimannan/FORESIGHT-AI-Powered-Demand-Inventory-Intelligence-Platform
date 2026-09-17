ce5e734c94a271d8c1b2b124b498cd0f4e6eefb2
Ansarimannan <168824140+Ansarimannan@users.noreply.github.com>
2026-09-17 18:53:33 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 )
 
 # API_URL = "http://127.0.0.1:8000"
-API_URL = "https://foresight-demand-inventory.onrender.com"
+API_URL = "https://foresight-api-5wxt.onrender.com"
 
 
 # ============================================================
